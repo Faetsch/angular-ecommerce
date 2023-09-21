8bf51a9cae5d86c44b7874d2e2a8f59af15c8030
Fatih Kesikli <fatihkesikli@gmail.com>
2023-09-21 19:09:50 +0200

    okta integration

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc: {
+        clientId: '0oabg48fq2PmH3GEf5d7',
+        issuer: 'https://dev-88136343.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
